506cb96ce4d46b355bd09f2f535b7cbc06d6e65b
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-05-10 17:03:20 +0530

    webinstall: Adapt to YAAP

--- src/config.js
+++ src/config.js
@@ -1,34 +1,14 @@
 export default {
     OS_NAME: process.env.VUE_APP_OS_NAME,
-    ACCENT_COLOR: "#0366d6",
+    ACCENT_COLOR: "#81007f",
     SUPPORTED_DEVICES: [
         {
-            name: "Pixel 4",
-            model: "flame",
-        },
-        {
-            name: "Pixel 4 XL",
-            model: "coral",
-        },
-        {
-            name: "Pixel 4a 5G",
-            model: "bramble",
-        },
-        {
-            name: "Pixel 5",
-            model: "redfin",
-        },
-        {
-            name: "Pixel 6",
-            model: "oriole",
-        },
-        {
-            name: "Pixel 6 Pro",
-            model: "raven",
+            name: "Poco F3/Mi 11X/Redmi K40",
+            model: "alioth",
         },
     ],
     RELEASE_VARIANTS: {
-        minimal: {
+        vanilla: {
             description: "Minimal version without Google apps or services.",
             suffix: "",
         },
@@ -36,14 +16,6 @@ export default {
             description:
                 "Version with Google services included, so the apps you’re familiar with will work. Recommended for most users.",
             suffix: " with Google services",
-        },
-        test: {
-            description: "Experimental version for testing only.",
-            suffix: "experimental",
-        },
-        beta: {
-            description: "Beta version for testing only.",
-            suffix: "beta",
         },
     },
     DONATION_LINKS: [
